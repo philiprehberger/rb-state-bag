841effd5ce58144701b3f518af5d50098c9a6181
Reverend Philip <me@philiprehberger.com>
2014-02-06 14:19:56 -0600

    Initial release: philiprehberger-state-bag

---
+++ README.md
@@ -0,0 +1,88 @@
+# philiprehberger-state_bag
+
+[![Tests](https://github.com/philiprehberger/rb-state-bag/actions/workflows/ci.yml/badge.svg)](https://github.com/philiprehberger/rb-state-bag/actions/workflows/ci.yml)
+[![Gem Version](https://badge.fury.io/rb/philiprehberger-state_bag.svg)](https://rubygems.org/gems/philiprehberger-state_bag)
+[![License](https://img.shields.io/github/license/philiprehberger/rb-state-bag)](LICENSE)
+
+Thread-local state bag for implicit context propagation
+
+## Requirements
+
+- Ruby >= 3.1
+
+## Installation
+
+Add to your Gemfile:
+
+```ruby
+gem 'philiprehberger-state_bag'
+```
+
+Or install directly:
+
+```bash
+gem install philiprehberger-state_bag
+```
+
+## Usage
+
+```ruby
+require 'philiprehberger/state_bag'
+
+Philiprehberger::StateBag.set(:user_id, 42)
+Philiprehberger::StateBag.get(:user_id)
+# => 42
+```
+
+### Default Values
+
+```ruby
+Philiprehberger::StateBag.get(:missing, 'fallback')
+# => "fallback"
+```
+
+### Scoped Overrides
+
+```ruby
+Philiprehberger::StateBag.set(:locale, 'en')
+
+Philiprehberger::StateBag.with(locale: 'de') do
+  Philiprehberger::StateBag.get(:locale)
+  # => "de"
+end
+
+Philiprehberger::StateBag.get(:locale)
+# => "en"
+```
+
+### Inspection
+
+```ruby
+Philiprehberger::StateBag.set(:a, 1)
+Philiprehberger::StateBag.key?(:a)   # => true
+Philiprehberger::StateBag.to_h       # => {:a=>1}
+Philiprehberger::StateBag.clear
+```
+
+## API
+
+| Method | Description |
+|--------|-------------|
+| `.set(key, val)` | Store a value in the thread-local state bag |
+| `.get(key, default = nil)` | Retrieve a value or return the default |
+| `.with(**overrides, &block)` | Execute block with temporary state, restoring after |
+| `.clear` | Remove all entries from the state bag |
+| `.to_h` | Return a snapshot of the current state |
+| `.key?(key)` | Check if a key exists in the state bag |
+
+## Development
+
+```bash
+bundle install
+bundle exec rspec
+bundle exec rubocop
+```
+
+## License
+
+MIT
---
+++ lib/philiprehberger/state_bag.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+require_relative 'state_bag/version'
+
+module Philiprehberger
+  module StateBag
+    class Error < StandardError; end
+
+    # Set a value in the thread-local state bag
+    #
+    # @param key [Symbol, String] the key to set
+    # @param val [Object] the value to store
+    # @return [Object] the stored value
+    def self.set(key, val)
+      store[key] = val
+    end
+
+    # Get a value from the thread-local state bag
+    #
+    # @param key [Symbol, String] the key to retrieve
+    # @param default [Object] optional default if key is not found
+    # @return [Object] the stored value or default
+    def self.get(key, default = nil)
+      store.fetch(key, default)
+    end
+
+    # Execute a block with temporary state, restoring previous values after
+    #
+    # @param overrides [Hash] key-value pairs to set during the block
+    # @yield the block to execute with the temporary state
+    # @return [Object] the return value of the block
+    def self.with(**overrides, &block)
+      previous = {}
+      missing = []
+
+      overrides.each do |k, v|
+        if store.key?(k)
+          previous[k] = store[k]
+        else
+          missing << k
+        end
+        store[k] = v
+      end
+
+      block.call
+    ensure
+      previous.each { |k, v| store[k] = v }
+      missing.each { |k| store.delete(k) }
+    end
+
+    # Clear all entries from the thread-local state bag
+    #
+    # @return [void]
+    def self.clear
+      store.clear
+    end
+
+    # Return a snapshot of the current state as a hash
+    #
+    # @return [Hash] copy of the current state
+    def self.to_h
+      store.dup
+    end
+
+    # Check if a key exists in the state bag
+    #
+    # @param key [Symbol, String] the key to check
+    # @return [Boolean] true if the key exists
+    def self.key?(key)
+      store.key?(key)
+    end
+
+    class << self
+      private
+
+      def store
+        Thread.current[:philiprehberger_state_bag] ||= {}
+      end
+    end
+  end
+end
